10e15c23217131761d773e01a736bc98a0566477
Raushan kumar <raushankumar8757825@gmail.com>
2025-06-16 11:08:06 +0530

    ch2 completed

--- hello_cargo/src/ch2/ch2.rs
+++ hello_cargo/src/ch2/ch2.rs
@@ -1,12 +1,43 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 pub fn ch2(){
      // we are making here is guessing game
+      let secret_number:i32=rand::thread_rng().gen_range(1..=100);
+      println!("secret number  {}",secret_number);
+    
+  loop{
+    let mut guess_number:String=String::new();
+    
+    io::stdin()
+    .read_line(&mut guess_number)
+    .expect("failed to take input");
 
-     let mut guess_number:String=String::new();
-     io::stdin()
-         .read_line(&mut guess_number)
-         .expect("failed to take input");
-
+   let guess_number:i32= match guess_number.trim().parse() {
+    Ok(num)=>num,
+    Err(_)=>continue,
+   };
+   
         println!("you number {}",guess_number);
+
+
+        match  guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("too less"),
+            Ordering::Equal=>{
+                println!("you won");
+                break;
+            }
+            Ordering::Greater=>println!("too greater"),
+            
+        }
+    }
 } 
+
+
+
+
+// what we learn in chapter 2 is the match which has similar  function like switch in c++,
+// we learn ok,expect method on function.
+// possibility of match uinsg cmp
+// matching of same datatype , here number only
+//concept of let which again assigned to same variable 
--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -3,13 +3,7 @@ mod ch2;
 use crate::ch1::ch1::ch1;
 use crate::ch2::ch2::ch2;
 fn main() {
-    let x = plus_one(5);
-
-    println!("The value of x is: {x}");
+    
     ch1();
     ch2();
-}
-
-fn plus_one(x: i32) -> i32 {
-    x + 1
 }
